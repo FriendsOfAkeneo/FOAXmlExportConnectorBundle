db8f79e161377925a11d532ae8140c9173c64818
NovikovViktor <viktor.novikov95@gmail.com>
2017-01-08 19:06:25 +0200

    Added service to render given xml template

--- Twig/TemplateRenderer.php
+++ Twig/TemplateRenderer.php
@@ -4,5 +4,27 @@ namespace FOA\XmlExportConnectorBundle\Twig;
 
 class TemplateRenderer
 {
+    public $kernelRootDir;
 
+    /**
+     * @param $kernelRootDir
+     */
+    public function __construct($kernelRootDir)
+    {
+        $this->kernelRootDir = $kernelRootDir;
+    }
+
+    /**
+     * @param $template
+     * @param $parameters
+     * @return mixed
+     */
+    public function renderTemplate($template, $parameters)
+    {
+        $templatesDir = sprintf('%s%s', $this->kernelRootDir, '/Resources/views/XMLTemplates');
+        $loader = new \Twig_Loader_Filesystem($templatesDir);
+        $twig   = new \Twig_Environment($loader);
+
+        return $twig->render($template, ['params' => $parameters]);
+    }
 }
